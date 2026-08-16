c325ba2f9969d8a50bd87c2988ed830d78cce2df
Sheridan Echternach <sheridan.hounschell@live.com>
2026-08-16 15:40:04 -0700

    Allow deployed frontend CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://pbis-rewards-1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
